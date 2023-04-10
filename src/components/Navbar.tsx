8f072cd270fddbf7db3715d24753e630106757ac
Shashank Verma <shashank.verma2002@gmail.com>
2023-04-10 18:55:46 +0530

    Navbar: Simplify route links
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import './styles.css'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+
+    const navGraph: [string, string][] = [
+        ['Home', '/'],
+        ['Clubs', '/clubs'],
+        ['Events', '/events'],
+        ['Achievements', '/achievements'],
+        ['Team', '/team'],
+    ]
+
     return (
         <div className="text-center text-white relative overflow-hidden bg-no-repeat bg-cover z-50">
             <nav className="flex bg-zinc-900 flex-col md:flex-row w-full fixed z-90 md:items-center justify-start md:justify-between md:flex-wrap p-4">
                 <div className="flex flex-row justify-between">
                     <div className="flex items-center flex-shrink-0 mr-6">
-                        <a href="#"><span className="font-bold text-2xl md:text-2xl tracking-tight">SAC MNNIT</span></a>
+                        <Link to='/'>
+                            <span className="font-bold text-2xl md:text-2xl tracking-tight">SAC MNNIT</span>
+                        </Link>
                     </div>
 
                     <div className="md:hidden flex justify-start">
@@ -21,25 +33,17 @@ const Navbar = () => {
 
                 <div className="mobile-menu block h-screen md:h-auto content-center text-xl md:text-sm text-center md:flex-grow lg:flex lg:items-center lg:w-auto">
                     <div className="lg:flex-grow">
-                        <a href="./index.php" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
-                            Home
-                        </a>
-                        <a href="./clubs.php" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
-                            Clubs
-                        </a>
-                        <a href="./events.php" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
-                            Events
-                        </a>
-                        <a href="./achievement.php" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
-                            Achievements
-                        </a>
-                        <a href="./team.php" className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
-                            Team
-                        </a>
+                        {
+                            navGraph.map(([name, path]) => (
+                                <Link to={path} className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
+                                    {name}
+                                </Link>
+                            ))
+                        }
                     </div>
 
                     <div>
-                        <a href="./login.php" className="inline-block text-sm px-4 py-2 leading-none font-medium border border-white rounded hover:border-transparent hover:bg-white hover:text-black mt-4 lg:mt-0 hover:animate-pulse">Admin Login</a>
+                        <a href="#" className="inline-block text-sm px-4 py-2 leading-none font-medium border border-white rounded hover:border-transparent hover:bg-white hover:text-black mt-4 lg:mt-0 hover:animate-pulse">Admin Login</a>
                     </div>
                 </div>
             </nav>
