ddf7464b79bcdd9b2b5f34ee40980456f2d0cccf
Shashank Verma <shashank.verma2002@gmail.com>
2023-04-11 18:45:40 +0530

    components: Footer: Implement scroll to top button
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { images } from '../models/images'
 import { Link } from 'react-router-dom'
+import { images } from '../models/images'
 
 const Footer: React.FC = () => {
+
+    const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+    const handleScroll = () => {
+        const toTopButton = document.getElementById('to-top-button')
+        if (toTopButton) {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                toTopButton.classList.remove('hidden')
+                toTopButton.classList.add('flex')
+            } else {
+                toTopButton.classList.add('hidden')
+                toTopButton.classList.remove('flex')
+            }
+        }
+    }
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, []);
+
     return (
         <div>
-            <button id="to-top-button" title="Go To Top" className="hidden fixed z-50 bottom-8 right-8 border-0 w-12 h-12 animate-bounce rounded-full drop-shadow-md bg-zinc-900 text-white text-2xl font-bold">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="24px" width="24px" viewBox="0 0 384 512">
+            <button id="to-top-button" title="Go To Top" className="justify-center content-center hidden fixed z-50 bottom-8 right-8 border-0 w-12 h-12 animate-bounce rounded-full drop-shadow-md bg-zinc-900 text-white text-2xl font-bold" onClick={scrollToTop}>
+                <svg xmlns="http://www.w3.org/2000/svg" className='' fill="currentColor" height="24px" width="24px" viewBox="0 0 384 512">
                     <path className="" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
                 </svg>
             </button>
