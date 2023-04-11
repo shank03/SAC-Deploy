acb3d377860bd84baa6bc7bae5f58cbb83498a67
Shashank Verma <shashank.verma2002@gmail.com>
2023-04-11 17:27:23 +0530

    components: Make navbar responsive
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import './styles.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
+
+    const [navbar, setNavbar] = React.useState(false)
 
     const navGraph: [string, string][] = [
         ['Home', '/'],
@@ -13,41 +14,63 @@ const Navbar: React.FC = () => {
     ]
 
     return (
-        <div className="text-center text-white relative overflow-hidden bg-no-repeat bg-cover z-50">
-            <nav className="flex bg-zinc-900 flex-col md:flex-row w-full fixed z-90 md:items-center justify-start md:justify-between md:flex-wrap p-4">
-                <div className="flex flex-row justify-between">
-                    <div className="flex items-center flex-shrink-0 mr-6">
-                        <Link to='/'>
+        <nav className="text-center text-white relative overflow-hidden bg-no-repeat bg-cover z-50">
+            <div className="justify-between bg-zinc-900 px-4 mx-auto md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-4 md:py-4 md:block">
+                        <NavLink to='/'>
                             <span className="font-bold text-2xl md:text-2xl tracking-tight">SAC MNNIT</span>
-                        </Link>
-                    </div>
-
-                    <div className="md:hidden flex justify-start">
-                        <button className="outline-none mobile-menu-button">
-                            <svg className="w-6 h-6 text-white hover:font-bold" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                <path d="M4 6h16M4 12h16M4 18h16"></path>
-                            </svg>
-                        </button>
+                        </NavLink>
+                        <div className="md:hidden">
+                            <button className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}>
+                                {navbar ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 20 20" fill="currentColor">
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
                     </div>
                 </div>
 
-                <div className="mobile-menu block h-screen md:h-auto content-center text-xl md:text-sm text-center md:flex-grow lg:flex lg:items-center lg:w-auto">
-                    <div className="lg:flex-grow">
+                <div className={`flex-1 justify-self-center pb-3 mt-2 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"}`}>
+                    <ul className="items-center justify-center md:flex md:space-y-0 text-xl md:text-sm text-center">
                         {
                             navGraph.map(([name, path]) => (
-                                <Link to={path} className="block mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse mr-4 font-medium">
-                                    {name}
-                                </Link>
+                                <li className={`text-white hover:text-gray-200 ${navbar ? 'mt-4' : ''}`}>
+                                    <NavLink to={path} className={`mt-4 lg:inline-block lg:mt-0 hover:font-bold hover:animate-pulse font-medium ${navbar ? '' : 'mr-4'}`} onClick={() => { if (navbar) setNavbar(!navbar) }}>
+                                        {name}
+                                    </NavLink>
+                                </li>
                             ))
                         }
-                    </div>
+                    </ul>
 
-                    <div>
-                        <a href="/" className="inline-block text-sm px-4 py-2 leading-none font-medium border border-white rounded hover:border-transparent hover:bg-white hover:text-black mt-4 lg:mt-0 hover:animate-pulse">Admin Login</a>
+                    <div className={`mt-4 mb-4 w-full space-y-2 lg:hidden ${navbar ? '' : 'hidden'}`}>
+                        <a href="/" className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100">
+                            Admin Login
+                        </a>
                     </div>
                 </div>
-            </nav>
-        </div>
+                <div className="hidden space-x-2 md:inline-block">
+                    <a href="/" className={`${navbar ? '' : ''} text-sm px-4 py-2 leading-none font-medium border border-white rounded hover:border-transparent hover:bg-white hover:text-black mt-4 lg:mt-0 hover:animate-pulse`}>
+                        Admin Login
+                    </a>
+                </div>
+            </div>
+        </nav >
     )
 }
 
